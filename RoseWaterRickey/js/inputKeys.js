7b2de4f669ee7d469e6cc816f357a062d86fc402
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2015-07-27 10:40:52 +0300

    Adding inputKeys.js consistency

--- RoseWaterRickey/js/inputKeys.js
+++ RoseWaterRickey/js/inputKeys.js
@@ -1,16 +1,17 @@
 function getBatmanKeys (game) {
-	var keys = game.input.keyboard.createCursorKeys();
-	keys.fire = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-	return keys;
+	return {
+        up: game.input.keyboard.addKey(Phaser.Keyboard.UP),
+        left: game.input.keyboard.addKey(Phaser.Keyboard.LEFT),
+        right: game.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
+        fire: game.input.keyboard.addKey(Phaser.Keyboard.DOWN),
+    };
 }
 
 function getSupermanKeys (game) {
     return {
         up: game.input.keyboard.addKey(Phaser.Keyboard.W),
-        down: game.input.keyboard.addKey(Phaser.Keyboard.S),
         left: game.input.keyboard.addKey(Phaser.Keyboard.A),
         right: game.input.keyboard.addKey(Phaser.Keyboard.D),
-        fire: game.input.keyboard.addKey(Phaser.Keyboard.M)
+        fire: game.input.keyboard.addKey(Phaser.Keyboard.S)
     };
 }
