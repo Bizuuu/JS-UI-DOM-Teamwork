6d925afffa158a4c8471a4900eabda00ebc693e1
Bianca Hinova <bianca.hinova@gmail.com>
2015-07-29 17:53:24 +0300

    Game Over in progress

--- RoseWaterRickey/js/gameOver.js
+++ RoseWaterRickey/js/gameOver.js
@@ -19,12 +19,22 @@ Game.GameOver.prototype = (function () {
             this.game.load.image('texts', 'imgs/gameOver/texts.png');
 
             this.game.load.audio('stamp', ['audio/stamp.mp3']);
+
+
         },
         create: function () {
-            var batmanGO;
+            var batmanGO,
+                supermanGO;
             this.game.add.sprite(0, 0, 'background');
-            batmanGO = this.game.add.sprite(0, 0, 'batman');
-            batmanGO.scale.setTo(2, -2);
+            batmanGO = this.game.add.sprite(80, 460, 'batman');
+            batmanGO.anchor.setTo(0, 0.9);
+            batmanGO.angle = -50;
+            this.game.add.tween(batmanGO).to({angle: 0}, 100, Phaser.Easing.Linear.None, true);
+            this.game.add.tween(batmanGO.scale).to({x: 0.95, y: 0.95}, 100, Phaser.Easing.Linear.None, true);
+            supermanGO = this.game.add.sprite(600, 1, 'superman');
+            supermanGO.anchor.setTo(0.9, 0);
+
+
             music = this.game.add.audio('stamp');
 
 
@@ -34,7 +44,7 @@ Game.GameOver.prototype = (function () {
                 winnerStampX = 120;   //120
             }
             this.game.time.events.add(Phaser.Timer.SECOND * 0.5, this.announceWinner, this.game);
-            this.game.add.button(270, 500, 'menu', this.returnToMenu, this.game);
+            this.game.add.button(300, 455, 'menu', this.returnToMenu, this.game).scale.setTo(0.75, 0.75);
 
 
         },
